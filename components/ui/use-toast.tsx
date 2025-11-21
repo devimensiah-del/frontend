78d6a0ce97afa903d8f9da5ff992279ddd740822
pradord <pradord@rose-hulman.edu>
2025-11-21 05:01:03 -0500

    fix: remove duplicate Toast interface that restricted variants

--- components/ui/use-toast.tsx
+++ components/ui/use-toast.tsx
@@ -140,14 +140,6 @@ function dispatch(action: Action) {
   })
 }
 
-interface Toast {
-  id: string
-  title?: React.ReactNode
-  description?: React.ReactNode
-  action?: ToastActionElement
-  variant?: "default" | "destructive"
-}
-
 function toast({ ...props }: Omit<ToasterToast, "id">) {
   const id = genId()
 
